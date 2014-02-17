efd971ea6ac56e383a2932d3f25f82f77c388bf3
Deck Y <my@mail.ru>
2014-02-17 17:22:13 +0400

    initial commit

---
+++ str-query-processor/src/main/java/sed/jettys/DBUtil.java
@@ -0,0 +1,96 @@
+package sed.jettys;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import sed.jettys.Jetty8901.CookieInfo;
+
+public class DBUtil {
+	
+    static Connection connection;
+    static String tableName = "theTable";
+    {
+
+    }
+
+	public static void writeCookie(CookieInfo ci) {
+		
+    	try {
+    		
+    		if (connection == null){
+    	        initConnection();
+    		}
+    		
+            Statement s = connection.createStatement();
+
+            String id = ci.id;
+            String cookie = ci.cookie;
+            String ipAddr = ci.ip;
+            String date = ci.date;
+//            System.out.println("insert into " + tableName + " values ('" + id + "','" + cookie + "','" + ipAddr + "','" + date + "')");
+
+            s.execute("insert into " + tableName + " values ('" + id + "','" + cookie + "','" + ipAddr + "','" + date + "')");
+
+            s.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+	}
+
+	private static void initConnection() {
+		try {
+		    Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
+		    connection = DriverManager.getConnection("jdbc:derby:sampleDB;create=true");
+		    try {
+		        Statement s = connection.createStatement();
+		        s.execute("CREATE TABLE " + tableName
+		                + " ( Id varchar(255), Cookie varchar(255),  IpAddr varchar(255), Date varchar(255)  )");
+		        s.close();
+		    } catch (SQLException e) {
+		        System.out.println("exist allready");
+		    }
+
+		} catch (InstantiationException e) {
+		    e.printStackTrace();
+		} catch (IllegalAccessException e) {
+		    e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+		    e.printStackTrace();
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		}
+	}
+
+	public static void dbClose() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String showInfo() {
+        Statement s;
+        StringBuilder sb = new StringBuilder();
+		try {
+    		if (connection == null){
+    	        initConnection();
+    		}
+
+			s = connection.createStatement();
+			ResultSet rs = s.executeQuery("select * from " + tableName);
+	        while (rs.next()) {
+	            sb.append ("[" + rs.getString(1) + "][" + rs.getString(2) + "][" + rs.getString(3) + "][" + rs.getString(4) + "]\n");
+	        }
+	        s.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
+
+}
---
+++ str-query-processor/src/main/java/sed/jettys/Jetty8901.java
@@ -0,0 +1,126 @@
+/**
+ * 
+ */
+package sed.jettys;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author edik
+ */
+public class Jetty8901 extends AbstractHandler {
+
+	private static boolean serverStop = false;
+
+	public static void main(String[] args) throws Exception {
+
+
+
+    	String jetty_home = System.getProperty("jetty.home","../..");
+    	 
+        Server server8902 = new Server(8902);
+ 
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/");
+        webapp.setWar(jetty_home+"/test-gwt-from-command/target/test-gwt-from-command-1.0-SNAPSHOT.war");
+        server8902.setHandler(webapp);
+
+
+		Server server = new Server(8901);
+		Jetty8901 handler = new Jetty8901();
+		server.setHandler(handler);
+		server.start();
+		server8902.start();
+		while (!serverStop) {
+			Thread.sleep(1234);
+		}
+		DBUtil.dbClose();
+		server8902.stop();
+		server.stop();
+	}
+
+	public void handle(String target, Request br, HttpServletRequest req,
+			HttpServletResponse resp) throws IOException, ServletException {
+		if ("/quit".equals(target)) {
+			serverStop = true;
+			br.setHandled(true);
+		} else if ("/favicon.ico".equals(target)) {
+			br.setHandled(true);
+		} else if ("/showInfo".equals(target)) {
+			resp.getWriter().write(DBUtil.showInfo());
+			br.setHandled(true);
+		} else {
+			CookieInfo cookieInfo = getSetCookie(br);
+			saveCookieInfo(cookieInfo);
+			br.setHandled(true);
+		}
+	}
+
+	private void saveCookieInfo(CookieInfo ci) {
+
+		DBUtil.writeCookie(ci);
+
+	}
+
+	private CookieInfo getSetCookie(Request br) {
+
+		CookieInfo ci = new CookieInfo();
+		ci.id = br.getRequestURI();
+		ci.ip = br.getRemoteAddr();
+		ci.port = "" + br.getRemotePort();
+
+		Cookie cookie = null;
+		if (br.getHeader("Cookie") != null) {
+			List<String> cookies = Arrays.asList(br.getHeader("Cookie")
+					.replaceAll("=", "").split("[;][\\s?]"));
+			Arrays.asList(br.getCookies()).iterator();
+			for (String cook : cookies) {
+				if (cook.contains("s.e.d-"
+						+ br.getRequestURI().replaceAll("/", "") + "-")) {
+					cookie = new Cookie(cook, "");
+				}
+			}
+		}
+
+		if (cookie == null) {
+			cookie = new Cookie("s.e.d-"
+					+ br.getRequestURI().replaceAll("/", "") + "-"
+					+ br.getRemoteAddr() + "-" + System.currentTimeMillis(), "");
+		}
+
+		cookie.setMaxAge(60 * 60 * 24 * 30 * 12);
+		br.getResponse().addCookie(cookie);
+		ci.cookie = cookie.getName();
+
+		return ci;
+	}
+
+	class CookieInfo {
+		String id;
+		String ip;
+		String port;
+		String cookie;
+		String date = "" + new Date();
+
+		@Override
+		public String toString() {
+			return "CookieInfo [id=" + id + ", ip=" + ip + ", port=" + port
+					+ ", cookie=" + cookie + ", date=" + date + "]";
+		}
+
+	}
+
+}
