dcffd625989d905cd14635cc0f2b7599fd041606
Edik Home <deck@mail.ru>
2014-02-17 23:15:33 +0400

    auto generation files added

---
+++ str-shower/src/main/java/sed/client/Messages.java
@@ -0,0 +1,26 @@
+package sed.client;
+
+/**
+ * Interface to represent the messages contained in resource bundle:
+ * 	C:/WspSS/str-machine/str-shower/src/main/resources/sed/client/Messages.properties'.
+ */
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  /**
+   * Translated "Enter your name".
+   * 
+   * @return translated "Enter your name"
+   */
+  @DefaultMessage("Enter your name")
+  @Key("nameField")
+  String nameField();
+
+  /**
+   * Translated "Send".
+   * 
+   * @return translated "Send"
+   */
+  @DefaultMessage("Send")
+  @Key("sendButton")
+  String sendButton();
+}
